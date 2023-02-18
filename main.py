a3993b065fd31db5796593f3d9aef03df00a5589
mdaugavietis <martins.daugavietis@edu.rtu.lv>
2023-02-18 20:13:24 +0200

    Bugfix

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def find_mismatch(text):
                ):
                 opening_brackets_stack.pop()
             else:
-                return i+1
+                return prev.position+1
     if len(opening_brackets_stack) > 0:
         return len(opening_brackets_stack)
     else:
