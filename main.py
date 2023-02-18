52ba3dbe5b84fe6311d4b9c0a835f9af5bcfa1eb
mdaugavietis <martins.daugavietis@edu.rtu.lv>
2023-02-18 21:07:48 +0200

    Bugfix #2

--- main.py
+++ main.py
@@ -6,28 +6,41 @@ Bracket = namedtuple("Bracket", ["char", "position"])
 
 def find_mismatch(text):
     opening_brackets_stack = []
+    brackets = 0
     for i, next in enumerate(text):
         if next in "([{":
-            opening_brackets_stack.append(Bracket(next, i+1))
+            head = Bracket(next, i+1)
+            opening_brackets_stack.append(head)
+            brackets += 1
 
         if next in ")]}":
             if len(opening_brackets_stack) == 0:
                 return i+1
-            prev = opening_brackets_stack[len(opening_brackets_stack)-1]
-            if ((prev.char == '(' and next == ')') or
-                (prev.char == '[' and next == ']') or
-                (prev.char == '{' and next == '}')
+            if ((head.char == '(' and next == ')') or
+                (head.char == '[' and next == ']') or
+                (head.char == '{' and next == '}')
                ):
                 opening_brackets_stack.pop()
             else:
-                return prev.position+1
-    if len(opening_brackets_stack) > 0:
-        return len(opening_brackets_stack)
+                return i+1
+            brackets -= 1
+            if brackets > 0:
+                head = opening_brackets_stack[len(opening_brackets_stack)-1]
+    if brackets > 0:
+        return brackets 
     else:
         return -1
 
 def main():
-    text = input()
+    mode = input()
+    if mode == "I":
+        text = input()
+    elif mode == "F":
+        with open(input(), "r") as f:
+            text = f.read()
+    else:
+        print("No mode selected")
+        return
     mismatch = find_mismatch(text)
     if mismatch == -1:
         print("Success")
